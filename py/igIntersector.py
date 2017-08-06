f182939e04ac3580357542d6015d4d6ea3134e9a
pletzera <pletzera@niwa-1007638v1.niwa.local>
2017-08-07 11:29:51 +1200

    + adding tests

--- py/igIntersector.py
+++ py/igIntersector.py
@@ -2,113 +2,119 @@ import vtk
 import numpy
 
 class Intersector:
-	"""
-	Compute the intersection between two grids
-	"""
+    """
+    Compute the intersection between two grids
+    """
 
-	TOL = 1.e-8
+    TOL = 1.e-8
 
-	def __init__(self, grid1, grid2):
-		self.cell2ToCell1 = self.getIntersections(grid1, grid2)
-		self.cell1ToCell2 = self.getIntersections(grid2, grid1)
+    def __init__(self, grid1, grid2):
+        self.cell2ToCell1 = self.getIntersections(grid1, grid2)
+        self.cell1ToCell2 = self.getIntersections(grid2, grid1)
 
-	def getCells1(self):
-		return self.cell2ToCell1
+    def getCells1(self):
+        return self.cell2ToCell1
 
-	def getCells2(self):
-		return self.cell1ToCell2
+    def getCells2(self):
+        return self.cell1ToCell2
 
-	def getIntersections(self, grida, gridb):
+    def getIntersections(self, grida, gridb):
 
-		cellBToCellAList = {}
-		cellLocator = vtk.vtkCellLocator()
-		cellLocator.SetDataSet(grida)
-		cellLocator.BuildLocator()
-		ptIds = vtk.vtkIdList()
-		p0 = numpy.zeros((3,), numpy.float64)
-		p1 = numpy.zeros((3,), numpy.float64)
-		cellAList = vtk.vtkIdList()
-		# iterate over the gridb cells
-		for iCellB in range(gridb.GetNumberOfCells()):
-			cellb = gridb.GetCell(iCellB)
-			# iterate over the edges of this cell
-			for iEdgeb in range(cellb.GetNumberOfEdges()):
-				edge = cellb.GetEdge(iEdgeb)
-				# find the intersection of this edge with grida
-				pts = edge.GetPoints()
-				p0[:] = pts.GetPoint(0)
-				p1[:] = pts.GetPoint(1)
-				cellLocator.FindCellsAlongLine(p0, p1, self.TOL, cellAList)
-				numCells = cellAList.GetNumberOfIds()
-				for i in range(numCells):
-					cellBToCellAList[iCellB] = cellBToCellAList.get(iCellB, []) + \
-						[cellAList.GetId(i)]
-		return cellBToCellAList
+        cellBToCellAList = {}
+        cellLocator = vtk.vtkCellLocator()
+        cellLocator.SetDataSet(grida)
+        cellLocator.BuildLocator()
+        ptIds = vtk.vtkIdList()
+        p0 = numpy.zeros((3,), numpy.float64)
+        p1 = numpy.zeros((3,), numpy.float64)
+        cellAList = vtk.vtkIdList()
+        # iterate over the gridb cells
+        for iCellB in range(gridb.GetNumberOfCells()):
+            cellb = gridb.GetCell(iCellB)
+            # iterate over the edges of this cell
+            for iEdgeb in range(cellb.GetNumberOfEdges()):
+                edge = cellb.GetEdge(iEdgeb)
+                # find the intersection of this edge with grida
+                pts = edge.GetPoints()
+                p0[:] = pts.GetPoint(0)
+                p1[:] = pts.GetPoint(1)
+                #print('start/end points: {} {}'.format(p0, p1))
+                cellLocator.FindCellsAlongLine(p0, p1, self.TOL, cellAList)
+                numCells = cellAList.GetNumberOfIds()
+                if numCells > 0 and iCellB not in cellBToCellAList:
+                    cellBToCellAList[iCellB] = set()
+                for i in range(numCells):
+                    #print('numCells = {} i = {} adding cell {}'.format(numCells, i, cellAList.GetId(i)))
+                    cellBToCellAList[iCellB].add(cellAList.GetId(i))
+        return cellBToCellAList
 
 #####################################################################################
 
 def createTriangulation(pts):
-	vPts = vtk.vtkPoints()
-	numPoints = len(pts)
-	vPts.SetNumberOfPoints(numPoints)
-	for i in range(numPoints):
-		vPts.SetPoint(i, pts[i])
-	poly = vtk.vtkPolyData()
-	poly.SetPoints(vPts)
-	delny = vtk.vtkDelaunay2D()
-	delny.SetInputData(poly)
-	delny.Update()
-	# convert the output of delny to a vtkUnstructuredGrid
-	appendFilter = vtk.vtkAppendFilter()
-	appendFilter.AddInputData(delny.GetOutput())
-	appendFilter.Update()
-	grid = vtk.vtkUnstructuredGrid()
-	grid.ShallowCopy(appendFilter.GetOutput())
-	print grid
-	return grid
+    vPts = vtk.vtkPoints()
+    numPoints = len(pts)
+    vPts.SetNumberOfPoints(numPoints)
+    for i in range(numPoints):
+        vPts.SetPoint(i, pts[i])
+    poly = vtk.vtkPolyData()
+    poly.SetPoints(vPts)
+    delny = vtk.vtkDelaunay2D()
+    delny.SetInputData(poly)
+    delny.Update()
+    # convert the output of delny to a vtkUnstructuredGrid
+    appendFilter = vtk.vtkAppendFilter()
+    appendFilter.AddInputData(delny.GetOutput())
+    appendFilter.Update()
+    grid = vtk.vtkUnstructuredGrid()
+    grid.ShallowCopy(appendFilter.GetOutput())
+    return grid
 
 def test0():
-	grid = createTriangulation([(0., 0., 0.), (1., 0., 0.), (0., 1., 0.)])
+    grid = createTriangulation([(0., 0., 0.), (1., 0., 0.), (0., 1., 0.)])
 
 def test1():
-	# create grid 1, one triangle
-	pts1 = vtk.vtkPoints()
-	pts1.SetNumberOfPoints(3)
-	pts1.SetPoint(0, [0., 0., 0.])
-	pts1.SetPoint(1, [1., 0., 0.])
-	pts1.SetPoint(2, [0., 1., 0.])
-	grid1 = vtk.vtkUnstructuredGrid()
-	grid1.SetPoints(pts1)
-	grid1.Allocate()
-	pt1Ids = vtk.vtkIdList()
-	pt1Ids.SetNumberOfIds(3)
-	pt1Ids.SetId(0, 0)
-	pt1Ids.SetId(1, 1)
-	pt1Ids.SetId(2, 2)
-	grid1.InsertNextCell(vtk.VTK_TRIANGLE, pt1Ids)
+    # 2 triangles, they overlap
+    grid1 = createTriangulation([(0., 0., 0.),
+                                 (1., 0., 0.),
+                                 (0., 1., 0.)])
+    grid2 = createTriangulation([(0., 0., 0.),
+                                 (1., 0., 0.),
+                                 (1., 1., 0.)])
 
-	# create grid 2, another triangle
-	pts2 = vtk.vtkPoints()
-	pts2.SetNumberOfPoints(3)
-	pts2.SetPoint(0, [0., 0., 0.])
-	pts2.SetPoint(1, [1., 0., 0.])
-	pts2.SetPoint(2, [1., 1., 0.])
-	grid2 = vtk.vtkUnstructuredGrid()
-	grid2.SetPoints(pts2)
-	grid2.Allocate()
-	pt2Ids = vtk.vtkIdList()
-	pt2Ids.SetNumberOfIds(3)
-	pt2Ids.SetId(0, 0)
-	pt2Ids.SetId(1, 1)
-	pt2Ids.SetId(2, 2)
-	grid2.InsertNextCell(vtk.VTK_TRIANGLE, pt2Ids)
+    insectr = Intersector(grid1, grid2)
+    print('test1: Cells of grid 2 intersecting cells of grid 1: {}'.format(insectr.getCells1()))
+    print('test1: Cells of grid 1 intersecting cells of grid 2: {}'.format(insectr.getCells2()))
 
-	insectr = Intersector(grid1, grid2)
-	print(insectr.getCells1())
-	print(insectr.getCells2())
+def test2():
+    # 2 triangles, no overlap but close by
+    grid1 = createTriangulation([(0., 0., 0.),
+                                 (1., 0., 0.),
+                                 (0., 1., 0.)])
+    grid2 = createTriangulation([(0., 1.2, 0.),
+                                 (1., 0.2, 0.),
+                                 (1., 1.2, 0.)])
+
+    insectr = Intersector(grid1, grid2)
+    print('test2: Cells of grid 2 intersecting cells of grid 1: {}'.format(insectr.getCells1()))
+    print('test2: Cells of grid 1 intersecting cells of grid 2: {}'.format(insectr.getCells2()))
+
+def test3():
+    # 2 triangles, no overlap and far away
+    grid1 = createTriangulation([(0., 0., 0.),
+                                 (1., 0., 0.),
+                                 (0., 1., 0.)])
+    grid2 = createTriangulation([(0., 2.2, 0.),
+                                 (1., 1.2, 0.),
+                                 (1., 2.2, 0.)])
+
+    insectr = Intersector(grid1, grid2)
+    print('test3: Cells of grid 2 intersecting cells of grid 1: {}'.format(insectr.getCells1()))
+    print('test3: Cells of grid 1 intersecting cells of grid 2: {}'.format(insectr.getCells2()))
 
 
 if __name__ == '__main__':
-	test0()
-	test1()
+    test0()
+    test1()
+    test2()
+    test3()
 
