f37e3fea7e99f64127f91377f876e4cf32cdaa06
Alex Pletzer <alexander@gokliya.net>
2017-08-29 08:42:37 +1200

    work in progress

--- py/igPiecewiseLinearLine.py
+++ py/igPiecewiseLinearLine.py
@@ -3,23 +3,23 @@ import numpy
 
 class PiecewiseLinearLine:
 
-	def __init__(self, lambdaFunction, thetaFunction, nt=11, radius=1.0):
-		"""
-		Create a polyline 
-		@param lambdaFunction lambda (longitude) function of t (0 <= t <= 1)
-		@param thetaFunction theta (latitude) functuon of t (0 <= t <= 1)
-		"""
-        self.ptData = vtk.DoubleArray()
+    def __init__(self, lambdaFunction, thetaFunction, nt=11, radius=1.0):
+        """
+        Create a polyline 
+        @param lambdaFunction lambda (longitude) function of t (0 <= t <= 1)
+        @param thetaFunction theta (latitude) functuon of t (0 <= t <= 1)
+        """
+        self.ptData = vtk.vtkDoubleArray()
         self.pts = vtk.vtkPoints()
-		self.line = vtk.vtkPolyLine()
+        self.line = vtk.vtkPolyLine()
         self.grid = vtk.vtkUnstructuredGrid()
 
-		ts = numpy.linspace(0., 1., nt)
-		lams = lambdaFunction(ts)
-		thes = thetaFunction(ts)
-		xs = radius * numpy.cos(thes) * numpy.cos(lams)
-		ys = radius * numpy.cos(thes) * numpy.sin(lams)
-		zs = radius * numpy.sin(thes)
+        ts = numpy.linspace(0., 1., nt)
+        lams = lambdaFunction(ts)
+        thes = thetaFunction(ts)
+        xs = radius * numpy.cos(thes) * numpy.cos(lams)
+        ys = radius * numpy.cos(thes) * numpy.sin(lams)
+        zs = radius * numpy.sin(thes)
         self.xyz = numpy.zeros((nt, 3), numpy.float64)
         self.xyz[:, 0] = xs
         self.xyz[:, 1] = ys
@@ -35,16 +35,16 @@ class PiecewiseLinearLine:
         ptIds = self.line.GetPointIds()
         ptIds.SetNumberOfIds(nt)
         for i in range(ptIds.GetNumberOfIds()):
-            segs.SetPointId(i, i)
+            ptIds.SetId(i, i)
 
         self.grid.SetPoints(self.pts)
         self.grid.Allocate(1, 1)
         # one cell
-        self.grid.InsertNextCell(self.line)
+        self.grid.InsertNextCell(self.line.GetCellType(), ptIds)
 
 
-	def save(self, filename):
-		"""
+    def save(self, filename):
+        """
         Write unstructured grid to file
         @param filename file name
         """
@@ -54,7 +54,7 @@ class PiecewiseLinearLine:
         writer.Update()
 
 ###############################################################################
-def main():
+def test():
     lam0, the0 = 165.0, -40.0
     a, b = 20.0, 15.0
     def lamFunc(ts):
@@ -64,6 +64,35 @@ def main():
 
     pl = PiecewiseLinearLine(lamFunc, theFunc, nt=21)
     pl.save('line.vtk')
+
+def main():
+    import argparse
+
+    parser = argparse.ArgumentParser(description='Create polyline')
+    parser.add_argument('--points', type=str, default='[(-180., 0.), (180., 0.)]', help='List of lon-lat points in degrees')
+    args = parser.parse_args()
+
+    pts = numpy.array(eval(args.points))
+
+    def lamFunc(ts):
+        i0 = numpy.array((ts - 1.e-10) // 1, numpy.int)
+        i1 = i0 + 1
+        xi = ts - i0
+        p0 = pts[i0, 0] * numpy.pi/180.
+        p1 = pts[i1, 0] * numpy.pi/180.
+        return  p0  + xi*(p1 - p0)
+
+    def theFunc(ts):
+        i0 = numpy.array((ts - 1.e-10) // 1, numpy.int)
+        i1 = i0 + 1
+        xi = ts - i0
+        p0 = pts[i0, 1] * numpy.pi/180.
+        p1 = pts[i1, 1] * numpy.pi/180.
+        return  p0  + xi*(p1 - p0)        
+
+    pl = PiecewiseLinearLine(lamFunc, theFunc, nt=len(pts))
+    pl.save('line.vtk')
+
 
     
 if __name__ == '__main__':
