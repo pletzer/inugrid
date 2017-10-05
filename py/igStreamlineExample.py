38aa04e31a3f42deb41286a168aa38e7fa1a0d45
pletzer <alexander@gokliya.net>
2017-10-05 01:27:29 +0000

    comparison between different interpolation methods

--- py/igStreamlineExample.py
+++ py/igStreamlineExample.py
@@ -14,8 +14,8 @@ v is interpolated using bilinear basis functions
 
 #  create the grid
 ns = (60, 30, 1)
-ls = (2*numpy.pi, 2.0, 1.0)
-origin = (0., -1.0, -0.5)
+ls = (2*numpy.pi, 3.0, 1.0)
+origin = (0., -1.5, -0.5)
 hs = (ls[0]/float(ns[0]), ls[1]/float(ns[1]), ls[2]/float(ns[2]))
 cart = CartesianGrid(ns, ls, origin)
 grid = cart.getUnstructuredGrid()
@@ -24,6 +24,9 @@ geom = GridGeometry(grid)
 
 
 def velocityExact(x, *args):
+    """
+    Exact velocity field
+    """
     # -d psi/dx
     vy = -2. * numpy.sin(x[0]) * numpy.cos(x[0])
     # d psi/dy
@@ -31,6 +34,9 @@ def velocityExact(x, *args):
     return numpy.array([vx, vy, 0.])
 
 def velocityNodal(x, *args):
+    """
+    Velocity field linear linterpolated from the exact values at the nodes
+    """
 
     res = numpy.zeros((3,), numpy.float64)
 
@@ -65,6 +71,10 @@ def streamFuncExact(x):
     return numpy.sin(x[0])**2 + x[1]**2
 
 def velocityFace(x, *args):
+    """
+    Velocity field interpolated using the face centred basis functions and exact 
+    fluxes on cell faces
+    """
     res = numpy.zeros((3,), numpy.float64)
     # find the cell the contains the point
     found = geom.findCell(x)
@@ -90,23 +100,64 @@ def velocityFace(x, *args):
     res[0:2] = vx, vy
     return res
 
+def velocityFaceAsNodal(x, *args):
+    """
+    Use the face basis functions to approximate the nodal vector field
+    """
+    res = numpy.zeros((3,), numpy.float64)
+    # find the cell the contains the point
+    found = geom.findCell(x)
+    if not found:
+        # return zero veolocity if outside the domain
+        return res
+
+    # parametric coordinates are stored in inverse order
+    xis = (geom.pcoords[2], geom.pcoords[1])
+    # vertices of the cell
+    pts = geom.cell.GetPoints()
+
+    # depends on the indexing of the hex in VTK
+    verts = [pts.GetPoint(i) for i in (0, 4, 7, 3)]
+    # compute the velocity at the nodes from the face centred basis functions
+    velNodes = [velocityFace(v) for v in verts]
+
+    # bilinear interpolation of velocity at vertices
+    w0 = (1. - xis[0]) * (1. - xis[1])
+    w1 = (xis[0] - 0.) * (1. - xis[1])
+    w2 = (xis[0] - 0.) * (xis[1] - 0.)
+    w3 = (1. - xis[0]) * (xis[1] - 0.)
+
+    res = w0*velNodes[0] + w1*velNodes[1] + w2*velNodes[2] + w3*velNodes[3]
+
+    return res
+
+
 # initial condition
-x = numpy.array([numpy.pi/2. + 0.1, 0.2, 0.0])
+x = numpy.array([numpy.pi/2. + 0.1, 0.015, 0.0])
 
 # times
-ts = numpy.linspace(0., 10., 101)
+ts = numpy.linspace(0., 100., 1001)
 
 # solve
 solExact = odeint(velocityExact, x, ts)
-solNodal = odeint(velocityNodal, x, ts)
+solNodal = odeint(velocityFaceAsNodal, x, ts)
 solFace = odeint(velocityFace, x, ts)
 
 # plot
+fig = pylab.figure()
+ax = fig.add_subplot(111)
+
 pylab.plot(solExact[:, 0], solExact[:, 1], 'g-')
 pylab.plot(solNodal[:, 0], solNodal[:, 1], 'r-')
 pylab.plot(solFace[:, 0], solFace[:, 1], 'b-')
 pylab.legend(['exact', 'nodal', 'face'])
+pylab.plot([solExact[0, 0]], [solExact[0, 1]], 'k*')
 
+x = numpy.linspace(origin[0], origin[0] + ls[0], 101)
+y = numpy.linspace(origin[1], origin[1] + ls[1], 101)
+xx, yy = numpy.meshgrid(x, y)
+pylab.contour(x, y, numpy.sin(xx)**2 + yy**2, colors='k')
+ax.set_aspect('equal')
 
 #pylab.title('igStreamlineNodalExample')
 
