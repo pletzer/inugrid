60765d1c4bbb437b6d19f12c42a30595458e90f1
pletzer <alexander@gokliya.net>
2017-11-21 01:46:48 +0000

    + new test

---
+++ py/igFluxExample2.py
@@ -0,0 +1,57 @@
+from igFluxCalculator import FluxCalculator
+from igCubedSphere import CubedSphere
+import numpy
+
+"""
+Example showing how to compute the flux across stream function grad lambda, 
+ie the 2-form is d lambda ^ dr.
+"""
+
+def psi(x, y):
+    """
+    Stream function
+    x ~ longitude
+    """
+    return x
+
+def integralFunction(xa, ya, xb, yb):
+    """
+    Compute flux attached to edge 
+    x ~ longitude
+    y ~ latitude
+    a: starting point
+    b: ending point
+    """
+    return psi(xb, yb) - psi(xa, ya)
+
+n = 10
+cs = CubedSphere(n)
+cs.save('cs.vtk')
+grid = cs.getUnstructuredGrid()
+
+fc = FluxCalculator(grid, integralFunction)
+
+# number of contour segments
+numSegments = 100
+
+# start longitude 	
+lamMin, lamMax = 0.0, 0.5*2*numpy.pi
+
+# increment in lambda
+dx = (lamMax - lamMin) / numSegments
+
+# fixed latitude, close to the north pole
+the = 1.0 # 0.9* numpy.pi/2.
+
+# expression for the contour in lon-lat coordinates
+line = numpy.array([(lamMin + i*dx, the) for i in range(numSegments + 1)]).reshape(numSegments + 1, 2)
+fc.setLine(line)
+
+# compute the flux
+totFlux = fc.computeFlux()
+
+print('Total flux: {}'.format(totFlux))
+exactFlux  = psi(line[-1][0], line[-1][1]) - psi(line[0][0], line[0][1])
+print('Exact flux: {}'.format(exactFlux))
+#if hasattr(fc, 'polyline2Flux'): print('line 2 flux map: {}'.format(fc.polyline2Flux))
+
