852957505f62e971f9d14172d1fdcb6b7140a6be
Alex Pletzer <alexander@gokliya.net>
2018-02-06 10:16:50 +1300

    reviewed code

--- py/igShowOneForm.py
+++ py/igShowOneForm.py
@@ -41,15 +41,16 @@ def getLambdaTheta(point):
     return lam, the
 
 
-n = 50
-
-actors = []
+# resolution of cubed-sphere 
+n = 10
 
 cs = igCubedSphere.CubedSphere(n, radius=1.01)
 grid = cs.getUnstructuredGrid()
 numCells = grid.GetNumberOfCells()
 points = grid.GetPoints()
 
+# create the edges to value map. Use a dict to avoid duplication 
+# of edges when iterating over cells (not the most efficient)
 ptIdEdges2Val = {}
 ptIds = vtk.vtkIdList()
 p0 = numpy.array([0., 0., 0.])
@@ -58,35 +59,39 @@ for cellId in range(numCells):
     grid.GetCellPoints(cellId, ptIds)
     numPoints = ptIds.GetNumberOfIds()
     for i0 in range(numPoints):
-        i1 = (i0 + 1) % numPoints
+        i1 = (i0 + 1) % numPoints # close the loop
+        # get the points of the edge
         ptId0 = ptIds.GetId(i0)
         ptId1 = ptIds.GetId(i1)
         points.GetPoint(ptId0, p0)
         points.GetPoint(ptId1, p1)
         # determine orientation of the edge (i0, i1)
         # north is positive
-        # if not then east
+        # if lat diff is zero then east is positive
+        # might have to be careful about dateline (let's not worry at this point)
         lam0, the0 = getLambdaTheta(p0)
         lam1, the1 = getLambdaTheta(p1)
         e01 = (ptId0, ptId1)
         orientation = 1
         dTheta, dLambda = the1 - the0, lam1 - lam0
-        if dTheta < 0 or (Theta == 0. and dLambda < 0):
+        if dTheta < 0 or (abs(dTheta) < EPS and dLambda < 0):
+            # swap the point indices
             e01 = (ptId1, ptId0)
             orientation = -1
         dPsi = psi(lam1, the1) - psi(lam0, the0)
         # store
         ptIdEdges2Val[e01] = orientation * dPsi
 
+numEdges = len(ptIdEdges2Val)
+
 # create the edges
 edges = vtk.vtkCellArray()
-numEdges = len(ptIdEdges2Val)
+edges.Allocate(numEdges)
 values = vtk.vtkDoubleArray()
 values.SetNumberOfComponents(1)
 values.SetNumberOfTuples(numEdges)
-edges.Allocate(numEdges)
 ptIds = vtk.vtkIdList()
-ptIds.SetNumberOfIds(2)
+ptIds.SetNumberOfIds(2) # probably not needed
 index = 0
 for e01, val in ptIdEdges2Val.items():
     ptIds.SetId(0, e01[0])
