ef9a145c89f179935cba46b1502a67708cb6ff06
Alex Pletzer <alexander@gokliya.net>
2018-04-06 04:13:46 +0000

    + edge vector

--- viz/generateEdgeVectorFieldCellByCell.py
+++ viz/generateEdgeVectorFieldCellByCell.py
@@ -10,21 +10,33 @@ writer = vtk.vtkUnstructuredGridWriter()
 
 # number of cells (radial, poloidal)
 nr, nt = 5, 12
-rmin, rmax = 0., 2.
+rmin, rmax = 1.e-6, 2.
 tmin, tmax = 0., 2.*numpy.pi
 
 dr, dt = (rmax - rmin)/float(nr), (tmax - tmin)/float(nt)
 
-def vecFun(x, y, z):
-	eps = 1.e-8
-	r2 = x*x + y*y + eps
-	return numpy.array([-y/r2, x/r2, 0.])
+def psiFunc(x, y, z):
+	theta = math.atan2(y, x)
+	return theta
+
+def gradXi1(x, y, z):
+	r = math.sqrt(x*x + y*y)
+	rHat = numpy.array([x/r, y/r, 0.])
+	return rHat/dr
+
+def gradXi2(x, y, z):
+	r = math.sqrt(x*x + y*y)
+	tHat = numpy.array([-y/r, x/r, 0.])
+	return tHat/(r * dt)
 
 nr1, nt1 = nr + 1, nt + 1
 
-# build the points
+# build the points and the field
 # 4 points per cell
 points.SetNumberOfPoints(nr * nt * 4)
+data.SetNumberOfComponents(3)
+data.SetNumberOfTuples(nr * nt * 4)
+
 z = 0. # on z = 0 plane (2D)
 k = 0
 for i in range(nr):
@@ -39,28 +51,27 @@ for i in range(nr):
 		x11, y11 = r1*numpy.cos(t1), r1*numpy.sin(t1)
 		x01, y01 = r0*numpy.cos(t1), r0*numpy.sin(t1)
 
+		# field values on the edges
+		vs0 = psiFunc(x10, y10, z) - psiFunc(x00, y00, z)
+		vs1 = psiFunc(x11, y11, z) - psiFunc(x01, y01, z)
+		v0s = psiFunc(x01, y01, z) - psiFunc(x00, y00, z)
+		v1s = psiFunc(x11, y11, z) - psiFunc(x10, y10, z)
+
 		points.InsertPoint(k, (x00, y00, z))
+		data.SetTuple(k, vs0*gradXi1(x00, y00, z) + v0s*gradXi2(x00, y00, z))
 		k += 1
 
 		points.InsertPoint(k, (x10, y10, z))
+		data.SetTuple(k, vs0*gradXi1(x10, y10, z) + v1s*gradXi2(x10, y10, z))
 		k += 1
 
 		points.InsertPoint(k, (x11, y11, z))
+		data.SetTuple(k, vs1*gradXi1(x11, y11, z) + v1s*gradXi2(x11, y11, z))
 		k += 1
 
 		points.InsertPoint(k, (x01, y01, z))
+		data.SetTuple(k, vs1*gradXi1(x01, y01, z) + v0s*gradXi2(x01, y01, z))
 		k += 1
-
-# build the data
-data.SetNumberOfComponents(3)
-data.SetNumberOfTuples(nr * nt * 4)
-k = 0
-for i in range(nr):
-	for j in range(nt):
-		for el in range(4):
-			x, y, z = points.GetPoint(k)
-			data.SetTuple(k, vecFun(x, y, z))
-			k += 1
 
 # build the grid
 numCells = nr * nt
@@ -83,5 +94,5 @@ grid.GetPointData().SetScalars(data)
 
 # save to file
 writer.SetInputData(grid)
-writer.SetFileName('polarVectorCellByCell.vtk')
+writer.SetFileName('edgeVectorCellByCell.vtk')
 writer.Update()
