990f89969d1ecf753f3274d7ad87fcc9163fe46f
Alex Pletzer <alexander@gokliya.net>
2018-04-07 13:46:03 +1200

    + adding exact and avg fields to the same plot

--- viz/generateEdgeVectorFieldCellByCell.py
+++ viz/generateEdgeVectorFieldCellByCell.py
@@ -5,6 +5,8 @@ import math
 
 points = vtk.vtkPoints()
 vecData = vtk.vtkDoubleArray()
+vecDataExact = vtk.vtkDoubleArray()
+vecDataAvgCell = vtk.vtkDoubleArray()
 psiData = vtk.vtkDoubleArray()
 grid = vtk.vtkUnstructuredGrid()
 writer = vtk.vtkUnstructuredGridWriter()
@@ -13,21 +15,34 @@ writer = vtk.vtkUnstructuredGridWriter()
 nr, nt = 10, 32
 rmin, rmax = 0.01, 2.
 tmin, tmax = 0., 2.*numpy.pi
+amplitude = 0.3
 
 dr, dt = (rmax - rmin)/float(nr), (tmax - tmin)/float(nt)
 
 def psiFunc(r, t):
-	return t + 0.3*r*math.cos(t)
+	return t + amplitude*r*math.cos(t)
+
+def dPsiDr(r, t):
+	return amplitude*math.cos(t)
+
+def dPsiDtOverR(r, t):
+	return 1.0/r - amplitude*math.sin(t)
 
 def gradXi1(x, y, z):
-	r = math.sqrt(x*x + y*y)
-	rHat = numpy.array([x/r, y/r, 0.])
-	return rHat/dr
+	return rHat(x, y, z)/dr
 
 def gradXi2(x, y, z):
 	r = math.sqrt(x*x + y*y)
-	tHat = numpy.array([-y/r, x/r, 0.])
-	return tHat/(r * dt)
+	return tHat(x, y, z)/(r * dt)
+
+def rHat(x, y, z):
+	r = math.sqrt(x*x + y*y)
+	return numpy.array([x/r, y/r, 0.])
+
+def tHat(x, y, z):
+	r = math.sqrt(x*x + y*y)
+	return numpy.array([-y/r, x/r, 0.])
+
 
 nr1, nt1 = nr + 1, nt + 1
 
@@ -37,7 +52,15 @@ points.SetNumberOfPoints(nr * nt * 4)
 
 vecData.SetNumberOfComponents(3)
 vecData.SetNumberOfTuples(nr * nt * 4)
-vecData.SetName('velocity')
+vecData.SetName('velocityEdge')
+
+vecDataExact.SetNumberOfComponents(3)
+vecDataExact.SetNumberOfTuples(nr * nt * 4)
+vecDataExact.SetName('velocityExact')
+
+vecDataAvgCell.SetNumberOfComponents(3)
+vecDataAvgCell.SetNumberOfTuples(nr * nt)
+vecDataAvgCell.SetName('velocityAvgCell')
 
 psiData.SetNumberOfComponents(1)
 psiData.SetNumberOfTuples(nr * nt * 4)
@@ -45,6 +68,7 @@ psiData.SetName('potential')
 
 z = 0. # on z = 0 plane (2D)
 k = 0
+kCell = 0
 for i in range(nr):
 	r0 = rmin + i*dr
 	r1 = r0 + dr
@@ -62,31 +86,62 @@ for i in range(nr):
 		psi11 = psiFunc(r1, t1)
 		psi01 = psiFunc(r0, t1)
 
-		# field values on the edges
+
+		# integrated field values on the edges
 		vs0 = psi10 - psi00 # bottom
 		vs1 = psi11 - psi01 # top
 		v0s = psi01 - psi00 # left
 		v1s = psi11 - psi10 # right
 
+		# edge vector velocities evaluated at the cell corners
+		v00 = vs0*gradXi1(x00, y00, z) + v0s*gradXi2(x00, y00, z)
+		v10 = vs0*gradXi1(x10, y10, z) + v1s*gradXi2(x10, y10, z)
+		v11 = vs1*gradXi1(x11, y11, z) + v1s*gradXi2(x11, y11, z)
+		v01 = vs1*gradXi1(x01, y01, z) + v0s*gradXi2(x01, y01, z)
+
+		# exact velocity at the cell corners
+		v00Exact = dPsiDr(r0, t0)*rHat(x00, y00, z) + dPsiDtOverR(r0, t0)*tHat(x00, y00, z)
+		v10Exact = dPsiDr(r1, t0)*rHat(x10, y10, z) + dPsiDtOverR(r1, t0)*tHat(x10, y10, z)
+		v11Exact = dPsiDr(r1, t1)*rHat(x11, y11, z) + dPsiDtOverR(r1, t1)*tHat(x11, y11, z)
+		v01Exact = dPsiDr(r0, t1)*rHat(x01, y01, z) + dPsiDtOverR(r0, t1)*tHat(x01, y01, z)
+
 		points.InsertPoint(k, (x00, y00, z))
-		vecData.SetTuple(k, vs0*gradXi1(x00, y00, z) + v0s*gradXi2(x00, y00, z))
+		vecData.SetTuple(k, v00)
+		vecDataExact.SetTuple(k, v00Exact)
 		psiData.SetTuple(k, [psi00])
 		k += 1
 
 		points.InsertPoint(k, (x10, y10, z))
-		vecData.SetTuple(k, vs0*gradXi1(x10, y10, z) + v1s*gradXi2(x10, y10, z))
+		vecData.SetTuple(k, v10)
+		vecDataExact.SetTuple(k, v10Exact)
 		psiData.SetTuple(k, [psi10])
 		k += 1
 
 		points.InsertPoint(k, (x11, y11, z))
-		vecData.SetTuple(k, vs1*gradXi1(x11, y11, z) + v1s*gradXi2(x11, y11, z))
+		vecData.SetTuple(k, v11)
+		vecDataExact.SetTuple(k, v11Exact)
 		psiData.SetTuple(k, [psi11])
 		k += 1
 
 		points.InsertPoint(k, (x01, y01, z))
-		vecData.SetTuple(k, vs1*gradXi1(x01, y01, z) + v0s*gradXi2(x01, y01, z))
+		vecData.SetTuple(k, v01)
+		vecDataExact.SetTuple(k, v01Exact)
 		psiData.SetTuple(k, [psi01])
 		k += 1
+
+		# cell average
+		xCell = 0.25*(x00 + x10 + x11 + x01)
+		yCell = 0.25*(y00 + y10 + y11 + y01)
+
+		# average of the north/south edge fields. Note: vs0 and vs1 are the 
+		# line integrals of the field over the edge so need to divide
+		# by the edge length (dr)
+		vAvgCell = 0.5*(vs0/dr + vs1/dr)*rHat(xCell, yCell, z)
+		# average of the west/east edge fields. Edge length is r*dt
+		vAvgCell += 0.5*(v0s/(r0*dt) + v1s/(r1*dt))*tHat(xCell, yCell, z)
+
+		vecDataAvgCell.SetTuple(kCell, vAvgCell)
+		kCell += 1
 
 # build the grid
 numCells = nr * nt
@@ -105,6 +160,8 @@ for i in range(nr):
 
 grid.SetPoints(points)
 grid.GetPointData().AddArray(vecData)
+grid.GetPointData().AddArray(vecDataExact)
+grid.GetCellData().AddArray(vecDataAvgCell)
 grid.GetPointData().AddArray(psiData)
 
 
